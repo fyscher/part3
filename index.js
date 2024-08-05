04e5208baa12b2683aea5cf3e12630c270cd72d5
fyscher <fyscher.kraal@gmail.com>
2024-08-04 21:59:57 -0600

    complete to 3.17: refactor delete and put requests for mongodb, add error handling middleware

--- index.js
+++ index.js
@@ -3,45 +3,40 @@ const express = require('express');
 const morgan  = require('morgan');
 const cors    = require('cors');
 const Entry   = require('./models/entry') 
+const PORT    = process.env.PORT
 
 morgan.token('body', req => JSON.stringify(req.body))
 
 const app = express();
+
+const unknownEndpoint = (req, res) =>
+{
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, req, res, next) =>
+{
+  console.error(error.message)
+
+  if (error.name === 'CastError')
+  {
+    return res.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
 
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 app.use(express.static('dist'));
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (req, res) =>
 {
-  Entry.find({}).then(entry =>
-  {
-    res.json(entry)
-  })
+  Entry
+    .find({})
+    .then( entry => res.json(entry))
+    .catch(error => next(error))
 })
 
 app.get('/info', (req, res) =>
@@ -51,24 +46,53 @@ app.get('/info', (req, res) =>
 
 app.get('/api/persons/:id', (req, res) => 
   {
-    Entry.findById(req.params.id).then(entry => res.json(entry))
+    Entry
+      .findById(req.params.id)
+      .then( entry => 
+        ! entry
+        ? res.status(404).json({ error: 'no entry found' })
+        : res.json(entry))
+      .catch( error =>
+      {
+        console.log(error)
+        res.status(500).send({ error: 'malformatted id'})
+        next(error)
+      }
+      )
   })
 
 app.delete('/api/persons/:id', (req, res) =>
 {
-  const id = Number(req.params.id);
-  persons = persons.filter(p => p.id !== id)
-  res.status(204).end();
+  Entry
+    .findByIdAndDelete(req.params.id)
+    .then( result => 
+      res.status(204).end())
+    .catch( error => next(error) )
+})
+
+app.put('/api/persons/:id', (req, res, next) =>
+{
+  const body = req.body
+
+  const entry = 
+  {
+    name: body.name,
+    number: body.number,
+  }
+
+  Entry
+    .findByIdAndUpdate(req.params.id, entry, { new: true })
+    .then( updatedEntry =>
+    {
+      res.json(updatedEntry)
+    }
+    )
+    .catch( error => next(error) )
 })
 
 app.post('/api/persons', (req, res) =>
 {
   const body = req.body;
-
-  if (body.name === undefined)
-  {
-    return res.status(400).json({ error: 'name missing'})
-  }
 
   const entry = new Entry
   ({
@@ -76,16 +100,18 @@ app.post('/api/persons', (req, res) =>
     number: body.number,
   })
 
-  entry.save().then( saved => { res.json(saved) })
+  ! body.name || ! body.number
+  ? res.status(400).json({ error: 'must fill out both fields' })
+  : entry
+    .save()
+    .then( saved =>{ res.json(saved) })
+    .catch( error => { next(error) })
 })
-
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
-}
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT
+app.use(errorHandler)
+
 app.listen( PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
